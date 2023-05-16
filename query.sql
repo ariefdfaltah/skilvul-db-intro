e01ec7546eb84f3e2ec36194b419b0939c4d5e9e
ryognldi <rioaziz.ginaldi@gmail.com>
2023-05-15 14:21:14 -1200

    add query data

---
+++ query.sql
@@ -0,0 +1,13 @@
+  INSERT INTO `playlist_track` (`playlist_id`, `track_id`) VALUES
+    (222, 1),
+    (333, 2),
+    (444, 3),
+    (555, 4),
+    (666, 5),
+    (777, 6),
+    (888, 7),
+    (999, 8),
+    (1212, 9),
+    (1313, 10);
+
+SELECT * FROM playlist_track;

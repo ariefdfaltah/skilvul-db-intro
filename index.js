a2b4bc2f4ab043ded0d4cadb8c2f8f4f2605fd4f
Arief Ditia Faltah <arief.dfaltah@gmail.com>
2023-05-17 10:07:13 +0700

    add code api

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+const port = 3030
+const usersStatic = [
+    {
+        id: 1,
+        name: "Arief",
+        hobby: "Eat"
+    },
+    {
+        id: 2,
+        name: "Binto",
+        hobby: "Sleep"
+    }
+
+
+]
+
+let checkData = (req,res, next) => {
+    console.log(`Saya Mengecek Data Ini : ${req.body}`)
+    next()
+}
+
+app.use(checkData)
+
+app.get("/", (req, res) => {
+    res.send("Hello Nakama Skilvul, Ini Adalah Root")
+})
+
+app.get("/users", (req, res) => {
+    res.json(
+        usersStatic
+    )
+})
+
+app.get("/users/:id", (req, res) => {
+    let response = usersStatic[req.params.id - 1]
+    res.json(
+        response
+    )
+})
+
+app.post("/users", (req, res) => {
+    let response = {
+        id: usersStatic.length + 1,
+        name: req.body.name,
+        hobby: req.body.hobby
+    }
+    usersStatic.push(response)
+    res.json(response)
+})
+
+app.put("/users/:id", (req, res) => {
+    let incomingUpdateDate = {
+        id: req.params.id,
+        name: req.body.name,
+        hobby: req.body.hobby
+    }
+    usersStatic[req.params.id - 1] = incomingUpdateDate
+
+    res.json(usersStatic[req.params.id - 1])
+    
+})
+
+app.delete("/users/:id", (req, res) => {
+    usersStatic.splice(req.params.id - 1, 1)
+    res.status(204)
+    res.send()
+})
+
+app.listen(port, () => {
+    console.log(`This Application Run on Port : ${port}`)
+})
